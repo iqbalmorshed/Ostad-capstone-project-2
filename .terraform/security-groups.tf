83bf4e00ce2e40ce470b118d51b6a29736cf0623
Iqbal Morshed <iqbal.morshed24@gmail.com>
2026-03-09 00:19:10 -0400

    openend another port for prometheus

--- .terraform/security-groups.tf
+++ .terraform/security-groups.tf
@@ -88,6 +88,14 @@ resource "aws_security_group" "k8s" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Prometheus"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Grafana
   ingress {
     description = "Grafana"
