0d1f9238825ad5a28d8425f3a57081f727c086b7
Iqbal Morshed <iqbal.morshed24@gmail.com>
2026-03-08 23:26:16 -0400

    skipped rate limiting on health check

--- quick-hire/src/app.controller.ts
+++ quick-hire/src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { SkipThrottle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
 
   @Get()
+  @SkipThrottle()
   getHello(): string {
     return 'QuickHire app is running'
   }
